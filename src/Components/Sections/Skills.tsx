4b9e1f8db3a5c6527aaeb314b60e10aa6085244c
Avishek Roy <roy1807053@stud.kuet.ac.bd>
2024-09-03 05:23:01 +0600

    Day 3: Skills section design done but data sections for skills yet to be add

--- src/Components/Sections/Skills.tsx
+++ src/Components/Sections/Skills.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { skills } from "../../Data/Constant";
 import { Tilt } from "react-tilt";
-
-const Container = styled.div`
+const NeonColorEffect = keyframes`
+  0%, 100% {
+    text-shadow: 0 0 4px #7f03fc, 0 0 8px #7f03fc, 0 0 12px #03eeff;
+  }
+  50% {
+    text-shadow: 0 0 2px #1303fc, 0 0 4px #03eeff, 0 0 6px #03eeff;
+  }
+`;
+const ContainerDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   position: relative;
   z-index: 1;
   align-items: center;
+  
 `;
 
-const Wrapper = styled.div`
+const WrapperDiv = styled.div`
   position: relative;
   display: flex;
   justify-content: space-between;
@@ -24,21 +32,25 @@ const Wrapper = styled.div`
   @media (max-width: 960px) {
     flex-direction: column;
   }
+  
+    
 `;
 
-const Title = styled.div`
+const TitleDiv = styled.div`
   font-size: 52px;
   text-align: center;
   font-weight: 600;
   margin-top: 20px;
-  color: ${({ theme }) => theme.text_primary};
+  color: #fc03d7;
+  animation: ${NeonColorEffect} 3s infinite alternate;
   @media (max-width: 768px) {
     margin-top: 12px;
     font-size: 32px;
   }
+    
 `;
 
-const Desc = styled.div`
+const DescriptionDiv = styled.div`
   font-size: 18px;
   text-align: center;
   font-weight: 600;
@@ -46,22 +58,24 @@ const Desc = styled.div`
   @media (max-width: 768px) {
     font-size: 16px;
   }
+    
 `;
 
-const SkillsContainer = styled.div`
+const SkillsContainerDiv = styled.div`
   width: 100%;
   display: flex;
   flex-wrap: wrap;
   margin-top: 20px;
   gap: 50px;
   justify-content: center;
+  
 `;
 
-const Skill = styled.div`
+const SkillDiv = styled.div`
   width: 100%;
   max-width: 500px;
   background-color: rgba(17, 25, 40, 0.83);
-  border: 1px solid rgba(255, 255, 255, 0.125);
+  border: 1px solid rgba(201, 50, 128, 0.4);
   box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
   border-radius: 16px;
   padding: 18px 36px;
@@ -74,6 +88,7 @@ const Skill = styled.div`
     max-width: 330px;
     padding: 10px 36px;
   }
+    
 `;
 
 const SkillTitle = styled.div`
@@ -81,7 +96,7 @@ const SkillTitle = styled.div`
   font-weight: 600;
   margin-bottom: 20px;
   text-align: center;
-  color: ${({ theme }) => theme.text_secondary};
+  color: #d8db23;
 `;
 
 const SkillList = styled.div`
@@ -90,13 +105,14 @@ const SkillList = styled.div`
   flex-wrap: wrap;
   gap: 12px;
   margin-bottom: 20px;
+  
 `;
 
 const SkillItem = styled.div`
   font-size: 16px;
   font-weight: 400;
-  color: ${({ theme }) => theme.text_primary + 80};
-  border: 1px solid ${({ theme }) => theme.text_primary + 80};
+  color: #02f3f7;
+  border: 1px solid #540d2f;
   border-radius: 12px;
   padding: 12px 16px;
   display: flex;
@@ -115,28 +131,28 @@ const SkillItem = styled.div`
 `;
 
 const SkillImage = styled.img`
-  width: 24px;
-  height: 24px;
+  width: 30px;
+  height: 30px;
+  // background-color: black;
 `;
 
 const Skills: React.FC = () => {
   return (
-    <Container id="Skills">
-      <Wrapper>
-        <Title>Skills</Title>
-        <Desc
+    <ContainerDiv id="Skills">
+      <WrapperDiv>
+        <TitleDiv>Skills</TitleDiv>
+        <DescriptionDiv
           style={{
             marginBottom: "40px",
           }}
         >
-          Here are some of my skills on which I have been working on for the
-          past 3 years.
-        </Desc>
+          Here are some of my skills on which I have been working on or worked till now or have knowledge about them.
+        </DescriptionDiv>
 
-        <SkillsContainer>
+        <SkillsContainerDiv>
           {skills.map((skill, index) => (
-            <Tilt key={`skill-${index}`}>
-              <Skill>
+            <Tilt key={`skill-${index}`} >
+              <SkillDiv>
                 <SkillTitle>{skill.title}</SkillTitle>
                 <SkillList>
                   {skill.skills.map((item, index_x) => (
@@ -146,12 +162,12 @@ const Skills: React.FC = () => {
                     </SkillItem>
                   ))}
                 </SkillList>
-              </Skill>
+              </SkillDiv>
             </Tilt>
           ))}
-        </SkillsContainer>
-      </Wrapper>
-    </Container>
+        </SkillsContainerDiv>
+      </WrapperDiv>
+    </ContainerDiv>
   );
 };
 
